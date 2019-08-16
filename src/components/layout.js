1107dcda52ece9988f1da3f5b43dace9754f58ff
Tony Nguyen <me@codetony.com>
2019-08-16 09:07:42 -0700

    Removed some boilerplate code to prepare for beginning of portfolio

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,25 +7,13 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
-import "./layout.css"
+import "./normalize.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle={"CODETONY"} />
       <div
         style={{
           margin: `0 auto`,
@@ -35,11 +23,6 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Hi everyone, this is codetony!</h1>
+    <p>Welcome to your new site.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
