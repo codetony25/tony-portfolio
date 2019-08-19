3ad7b0cd710aa08e1d2e7a8f3dd6e4627c59e179
Tony Nguyen <me@codetony.com>
2019-08-19 15:00:07 -0700

    updated header styles with codetony.com link

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,13 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0, textAlign: "center" }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <header>
+    <div>
+      <h1>
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
   </header>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import Header from "./header"
@@ -13,7 +6,7 @@ import "./normalize.css"
 const Layout = ({ children }) => {
   return (
     <>
-      <Header siteTitle={"CODETONY"} />
+      <Header siteTitle={"codetony.com"} />
       <div
         style={{
           margin: `0 auto`,
