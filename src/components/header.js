b338965fdab0eda322df0dfead0ee33d40be9713
Tony Nguyen <me@codetony.com>
2019-08-20 09:49:27 -0700

    added links to header

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,34 @@ import "./header.css"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <div>
-      <h1>
-        <Link to="/">{siteTitle}</Link>
-      </h1>
+    <div className="left-nav">
+      <ul>
+        <li>
+          <Link to="/">portfolio</Link>
+        </li>
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+        <li>
+          <Link to="/contact">contact</Link>
+        </li>
+      </ul>
+    </div>
+    <div className="middle-nav">
+      <Link to="/">{siteTitle}</Link>
+    </div>
+    <div className="right-nav">
+      <ul>
+        <li>
+          <Link to="/">linkedIn</Link>
+        </li>
+        <li>
+          <Link to="/">twitter</Link>
+        </li>
+        <li>
+          <Link to="/">github</Link>
+        </li>
+      </ul>
     </div>
   </header>
 )
