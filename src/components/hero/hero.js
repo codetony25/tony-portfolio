01111bd624f32468fafdd102afb314b9dabed5ed
Tony Nguyen <me@codetony.com>
2019-09-03 09:21:44 -0700

    added skeleton to hero

---
+++ src/components/hero/hero.js
@@ -0,0 +1,15 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+class Hero extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div>Hero</div>
+  }
+}
+
+export default Hero
